44573a2f17ea1eb77fb414b06f792a4ac560206f
FedorVisa <floodywizard@gmail.com>
2020-10-05 23:05:29 +0300

    1 more  commit

---
+++ laba1/laba1.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+	int i = 0;
+	int max = 0;
+	int k = 0;
+	int n = 0;
+	printf(" type 0 to end");
+	while (scanf("%d", &i) == 1 && i != 0) {
+		k++;
+		if (i > max) {
+			max = i;
+			n = k;
+		}
+	}
+	printf("number of the max element = %d\n", n);
+	return 0;
+}
