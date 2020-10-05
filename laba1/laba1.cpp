d7750a1264ef5fbdeb839c7422d08f4fc03aa76d
FedorVisa <floodywizard@gmail.com>
2020-10-05 23:23:28 +0300

    dop  commit

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -1,18 +1,25 @@
 ﻿#include <stdio.h>
 
-int main() {
-	int i = 0;
-	int max = 0;
-	int k = 0;
-	int n = 0;
-	printf(" type 0 to end");
-	while (scanf("%d", &i) == 1 && i != 0) {
-		k++;
-		if (i > max) {
-			max = i;
-			n = k;
-		}
-	}
-	printf("number of the max element = %d\n", n);
-	return 0;
+
+ int main() {
+    int a[10], i, j, b,N=10;
+    
+    for (i = 0; i < N; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    for (i = 0; i < N - 1; i++) {
+        for (j = 0; j < N - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                b = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = b;
+            }
+        }
+    }
+
+    for (i = 0; i < N; i++) {
+        printf("%d", a[i]);
+        printf("\n");
+    }
 }
